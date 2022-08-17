80770accda9a2caefbd0db1afe8c0313ff5c0bf7
Chris Loggins <75412063+clogginsdev@users.noreply.github.com>
2022-08-17 18:00:32 -0500

    Removed anything that doesn't involve what I am building

--- src/ui/next/with-layout/index.jsx
+++ src/ui/next/with-layout/index.jsx
@@ -3,15 +3,15 @@ export default function withLayout(Page) {
     return (
       <div className="container mx-auto font-light text-white">
         <header className="">
-          <div className="mx-auto relative w-[260px] flex items-center justify-center ">
-            awesome app
+          <div className="mx-auto relative w-[100%] flex items-center justify-center bg-slate-700 p-4">
+            <span className="text-2xl font-bold">30 Components - 30 Days</span>
           </div>
         </header>
         <main className="">
           <Page>{children}</Page>
         </main>
-        <footer className="mt-16">
-          <p>our footer here</p>
+        <footer className="mt-[45%] w-[100%] bg-slate-800 p-6 text-center">
+          <p className="text-center text-blue-300 font-bold">If you find this useful, give us a star on  <a className="text-white" href="https://github.com/devmentorlive-youtube/30-components-in-30-days">Github</a></p>
         </footer>
       </div>
     );
