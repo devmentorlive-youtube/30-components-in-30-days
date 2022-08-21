3ac30ff2cb470ff469e6794d885d4727b34d52d5
Chris Loggins <75412063+clogginsdev@users.noreply.github.com>
2022-08-21 15:52:42 -0500

    Installed Prettier and fixed the imports for examples/inputs/with-alert

--- src/pages/examples/inputs/with-alert/index.jsx
+++ src/pages/examples/inputs/with-alert/index.jsx
@@ -1,19 +1,28 @@
 import TextField from "@/ui/form/text-field";
-import withLabel from "@/ui/form/text-field";
-
+import withLabel from "@/ui/form/with-label";
+import { useState } from "react";
 
 
 export default function InputWithAlertPage () {
-
+  const [input, setInput] = useState("");
   const InputWithLabel = withLabel(TextField);
+
+  const handleChange = () => {
+    setInput(event.target.value);
+  }
 
 return (
   <div className="container w-[60%] mx-auto bg-white text-black p-4 my-8 rounded">
-    <h1 className="text-2xl">Input With Alert Example</h1>
+                           <h1 className="text-2xl">Input With Alert Example</h1>
 
     <div className="border rounded border-slate-800 mt-4 p-4">
-    
-      <InputWithLabel label="" value="" onChange={() => {}}></InputWithLabel>
+        {/* create an input with label that sets the state of the input */}
+        <InputWithLabel
+            label="Input"
+            value={input}
+            onChange={handleChange}
+           // alert={input.length > 0 ? "Input is not empty" : "Input is empty"}
+        />
 
     </div>
   </div>
