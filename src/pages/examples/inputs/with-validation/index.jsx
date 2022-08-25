0620fbfaf053cb9aa17508fd30fc2c76eddaa516
Chris Loggins <75412063+clogginsdev@users.noreply.github.com>
2022-08-25 12:09:40 -0500

    Completed the button with Alert example

--- src/pages/examples/inputs/with-validation/index.jsx
+++ src/pages/examples/inputs/with-validation/index.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import TextField from "@/ui/form/text-field";
 import withLabel from "@/ui/form/with-label";
 import withValidation from "@/ui/form/with-validation";
-
+import Head from "next/head";
 
 // Wrapped Components
 const InputWithLabel = withLabel(TextField);
@@ -11,21 +11,28 @@ const InputWithValidation = withValidation(InputWithLabel);
 export default function InputWithAlert() {
   const [text, setText] = useState("");
   const [touched, setTouched] = useState(false);
-  const isGmail = (text) => text.includes("@gmail.com") ? "" : "Must be a gmail address"
+  const isGmail = (text) =>
+    text.includes("@gmail.com") ? "" : "Must be a gmail address";
 
   return (
-    <div className="container w-[60%] mx-auto bg-gray-200 text-black p-4 my-8 rounded">
-      <h1 className="text-2xl font-medium my-4">Input With Alert Example</h1>
-      <p>Try entering a non-gmail address</p>
-      <div className="border rounded border-slate-800 mt-4 p-4">
+    <div className="container w-[40%] mx-auto bg-gray-600 text-white p-8 my-8 rounded">
+      <Head>
+        <title>Input with validation</title>
+      </Head>
+      <h1 className="header-one">Input With Alert Example</h1>
+      <p className="font-light">Try entering a non-gmail address</p>
+      <div className="border rounded border-gray-800 mt-4 p-4">
         {/* create an input with label that sets the state of the input */}
-        <InputWithValidation value={text} 
-        validations={[isGmail]}
-        onChange={(value) => {setText(value)
-        setTouched(true)
-        }}>
-          <span className="text-blue-600">Enter Your Email</span>
-          </InputWithValidation>
+        <InputWithValidation
+          value={text}
+          validations={[isGmail]}
+          onChange={(value) => {
+            setText(value);
+            setTouched(true);
+          }}
+        >
+          <span className="text-white">Enter Your Email</span>
+        </InputWithValidation>
       </div>
     </div>
   );
--- src/ui/next/with-layout/index.jsx
+++ src/ui/next/with-layout/index.jsx
@@ -3,7 +3,7 @@ export default function withLayout(Page) {
     return (
       <div className="container mx-auto font-light text-white">
         <header className="">
-          <div className="mx-auto relative w-[100%] flex items-center justify-center bg-slate-700 p-4">
+          <div className="mx-auto relative w-[100%] flex items-center justify-center bg-gray-900 p-4">
             <span className="text-2xl font-bold">30 Components - 30 Days</span>
           </div>
         </header>
@@ -11,7 +11,15 @@ export default function withLayout(Page) {
           <Page>{children}</Page>
         </main>
         <footer className="mt-[45%] w-[100%] bg-slate-800 p-6 text-center">
-          <p className="text-center text-blue-300 font-bold">If you find this useful, give us a star on  <a className="text-white" href="https://github.com/devmentorlive-youtube/30-components-in-30-days">Github</a></p>
+          <p className="text-center text-blue-300 font-bold">
+            If you find this useful, give us a star on{" "}
+            <a
+              className="text-white"
+              href="https://github.com/devmentorlive-youtube/30-components-in-30-days"
+            >
+              Github
+            </a>
+          </p>
         </footer>
       </div>
     );
